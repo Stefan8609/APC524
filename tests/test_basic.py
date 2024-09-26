99d33bd12021d95e1c676e50d6fcfbd63254356c
Stefan P. Kildal-Brandt <143521888+Stefan8609@users.noreply.github.com>
2024-09-26 16:09:45 -0400

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,8 +2,8 @@ import example
 
 
 def test_compare():
-    a = example.vector(1,2)
-    b = example.vector(1,2)
+    a = example.Vector(1,2)
+    b = example.Vector(1,2)
 
     assert a.x == b.x
     assert a.y == b.y
