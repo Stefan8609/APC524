b54e2ecf70c0fc1744631d985764f55985f822c2
Stefan8609 <sk8609@princeton.edu>
2024-10-01 16:06:28 -0400

    edit: formatting change

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,8 +2,8 @@ import example
 
 
 def test_compare():
-    a = example.Vector(1,2)
-    b = example.Vector(1,2)
+    a = example.Vector(1, 2)
+    b = example.Vector(1, 2)
 
     assert a.x == b.x
     assert a.y == b.y
